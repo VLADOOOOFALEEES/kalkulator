c1e58026e6b409a5f513eda4692b9850ca1ce580
admin <admin@DESKTOP-85A2412>
2024-09-25 15:36:05 +0500

    while

--- while/while.cpp
+++ while/while.cpp
@@ -1,40 +1,25 @@
 #include <iostream>
 #include <Windows.h>
 
-int main(){
+int main() {
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
-	int a = 1;int b = 0;int c = 0;
-	while(a != 0) {
+	int a = 1; int b = 0; int c = 0;
+	while (a != 0) {
 		std::cout << "¬ведите любое число или ноль дл€ выхода: ";
 		std::cin >> a;
 		if (a % 2) {
 			b += a;
-			}
+		}
 		if (!(a % 2)) {
 			c += a;
-			}
 		}
+	}
 	std::cout << "—умма всех четных = " << c << "\n—умма всех нечетных чисел = " << b;
 
 
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
